c879dca9789b93dd45f23d05ce08ad77edd4bc29
Priyanshu0512 <priyanshuk0595@gmail.com>
2024-01-30 22:23:37 +0530

    Added nodemailer setup .

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
-const {Serverconfig,Logger} = require('./config');
+const {ServerConfig,Logger} = require('./config');
 const express = require('express');
 const app =express();
 const apiRoutes =require('./routes');
 
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+const mailSender = require('./config/email_config');
+
+
 app.use('/api',apiRoutes);
 
-app.listen(Serverconfig.PORT,function exec(){
-    console.log(`Successfully started the server on ${Serverconfig.PORT}`);
+app.listen(ServerConfig.PORT, async function exec(){
+    console.log(`Successfully started the server on ${ServerConfig.PORT}`);
     Logger.info("Success",{});
+    try {
+        const response = await mailSender.sendMail({
+            from: ServerConfig.GMAIL_EMAIL,
+            to: 'abc@gmail.com',
+            subject: 'is the service working?',
+            text: "hi"
+        });
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
 })
